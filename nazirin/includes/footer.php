82aebac0039d281bf15411ad290fe7d223170656
afiqzakwan <afiqzakwan.anuar@gmail.com>
2024-05-13 22:03:57 +0800

    Commit 1 - 13 May

---
+++ nazirin/includes/footer.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+
+<footer>
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-6">
+          <h6>About Us</h6>
+          <ul>
+
+
+          <li><a href="page.php?type=aboutus">About Us</a></li>
+            <li><a href="page.php?type=faqs">FAQs</a></li>
+            <li><a href="page.php?type=privacy">Privacy</a></li>
+            <li><a href="page.php?type=terms">Terms of use</a></li>
+            <li><a href="admin/">Admin Login</a></li>
+          </ul>
+        </div>
+
+      
+      </div>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-push-6 text-right">
+        </div>
+        <div class="col-md-6 col-md-pull-6">
+          <p class="copy-right">Copyright &copy; 2024</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
